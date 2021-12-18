8dc1cac2ed61a63357f356a418090f9a9d38784a
Theo Uys <theouys@outlook.com>
2021-12-18 12:11:23 +0200

    Comments added for SQL database

--- bookapi/Program.cs
+++ bookapi/Program.cs
@@ -5,8 +5,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Install dotnet-ef - dotnet tool install --global dotnet-ef
+//This is used for EntityFramework
+//Create migration scripts  dotnet ef migrations add InitialMigration (last word is a name for it)
+
+//AddScope - Create once per client
+//AddSingleton - Same for every request
+//Transient - New instance created everytime
 builder.Services.AddScoped<IBookRepository, BookRepository>();
-builder.Services.AddDbContext<BookContext>(o => o.UseSqlite("Data source=books.db"));
+builder.Services.AddDbContext<BookContext>(option => option.UseSqlite("Data source=books.db"));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
